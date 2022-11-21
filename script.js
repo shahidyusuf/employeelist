8f750abce387bdd7ac2ad0e5a3df6acaaae7ca25
M.Shahid <shd.92yus@gmail.com>
2022-11-21 22:40:40 +0500

    employlist

---
+++ script.js
@@ -0,0 +1,38 @@
+function onFormSubmit(){
+    // 2nd formulla..
+    var formData=readFormData();
+// 3rd function..
+insertNewRecord(formData);
+}
+    function readFormData(){
+        var formData={};
+        formData["fullName"]=document.getElementById("fullName").value;
+        formData["empCode"]=document.getElementById("empCode").value;
+        formData["salary"]=document.getElementById("salary").value;
+        formData["city"]=document.getElementById("city").value;
+        return formData;
+}
+function insertNewRecord(data){
+var table = document.getElementById("employeeList")
+.getElementsByTagName('tbody')[0];
+
+var newRow = table.insertRow(table.length);
+
+cell1=newRow.insertCell(0);
+cell1.innerHTML = data.fullName;
+
+cell2=newRow.insertCell(1);
+cell2.innerHTML = data.empCode;
+
+cell3=newRow.insertCell(2);
+cell3.innerHTML = data.salary;
+
+cell4=newRow.insertCell(3);
+cell4.innerHTML = data.city;
+
+cell4.newRow.insertCell(4);
+cell4.innerHTML=`<a>Edit</a> 
+                 <a>Delete</a>`
+                 
+}
+
